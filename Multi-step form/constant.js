5b70b11e2a7ee547f6fb59ec103fffc7fe4f2ede
Dhruv B <42473590+strygwyr69@users.noreply.github.com>
2023-02-13 13:09:57 +0530

    Updating code

---
+++ Multi-step form/constant.js
@@ -0,0 +1,6 @@
+const EMAIL_REGEX =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+const IN_MOBILE_REGEX = /^(\+91\-|0)?[789]\d{9}$/;
+
+export { EMAIL_REGEX, IN_MOBILE_REGEX };
